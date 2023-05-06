c378985c27947dfe12caa209cea5374826cc9888
HaticeGulten <haticegulten975@gmail.com>
2023-05-06 18:56:47 +0300

    login and address

--- app/models.py
+++ app/models.py
@@ -1,4 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATE_CHOICES = (
+    ('Adana', 'Adana'),
+    ('Adıyaman', 'Adıyaman'),
+    ('Afyonkarahisar', 'Afyonkarahisar'),
+    ('Ankara', 'Ankara'),
+    ('Antalya', 'Antalya'),
+    ('Aydın', 'Aydın'),
+    ('Balıkesir', 'Balıkesir'),
+    ('Bursa', 'Bursa'),
+    ('Çanakkale', 'Çanakkale'),
+    ('Denizli', 'Denizli'),
+    ('Edirne', 'Edirne'),
+    ('Eskişehir', 'Eskişehir'),
+    ('Gaziantep', 'Gaziantep'),
+    ('Hatay', 'Hatay'),
+    ('Isparta', 'Isparta'),
+    ('İstanbul', 'İstanbul'),
+    ('İzmir', 'İzmir'),
+    ('Kahramanmaraş', 'Kahramanmaraş'),
+    ('Kocaeli', 'Kocaeli'),
+    ('Konya', 'Konya'),
+    ('Kütahya', 'Kütahya'),
+    ('Manisa', 'Manisa'),
+    ('Mersin', 'Mersin'),
+    ('Muğla', 'Muğla'),
+    ('Sakarya', 'Sakarya'),
+    ('Samsun', 'Samsun'),
+    ('Sivas', 'Sivas'),
+    ('Trabzon', 'Trabzon'),
+    ('Uşak', 'Uşak'),
+    ('Zonguldak', 'Zonguldak'),
+)
 
 CATEGORY_CHOICES=(
     ('PN', 'Pasta-Napolitano'),
@@ -22,3 +56,14 @@ class Product(models.Model):
     product_image = models.ImageField(upload_to='product')
     def __str__(self):
         return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
+from . forms import CustomerRegistrationForm, CustomerProfileForm, Customer
+from django.contrib import messages
 
 def home(request):
     return render(request, 'app/home.html')
@@ -28,3 +30,50 @@ class ProductDetail(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html', locals())
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', locals())
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Tebrikler! Kaydınız Başarılı.")
+        else:
+            messages.warning(request, "Geçersiz Giriş.")
+        return render(request, 'app/customerregistration.html', locals())
+    
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', locals())
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'Tebrikler! Kullanıcı Başarıyla Oluşturuldu')
+        else:
+            messages.warning(request, "Geçersiz Kullanıcı Bilgileri")
+        return render(request, 'app/profile.html', locals())
+    
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html', locals())
+
+class updateAddress(View):
+    def get(self, request, pk):
+        form = CustomerProfileForm()
+        return render(request, 'app/updateAddress.html', locals())
+    def post(self, request, pk):
+        form = CustomerProfileForm(request.POST)
+        return render(request, 'app/updateAddress.html', locals())
